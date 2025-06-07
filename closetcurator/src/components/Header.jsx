a23256ce5fed1af10a9a8c78eecd2113704ffd85
Ameya Patkar <ameyaspatkar@gmail.com>
2025-06-06 19:25:29 -0700

    Converted vanilla JS to React

---
+++ closetcurator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Closet Curator</h1>
+        <h3> Enter Your Clothing Likes and Dislikes </h3>
+    </div>
+  )
+}
+
+export default Header;
---
+++ closetcurator/src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <form>
+        <textarea placeholder="Enter a clothing feature you like"></textarea>
+        <button id="submit">SUBMIT</button>
+    </form>
+  )
+}
+
+export default Input;
